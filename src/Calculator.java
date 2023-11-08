7c597ab12fdeb6a1488eb4b4df02e6c341474421
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2023-11-08 13:50:47 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,33 +3,36 @@ public class Calculator {
     private int total;
     private int value;
 
+    //Post: Instance variables total and value are set to startingValue and 0 respectively
     public Calculator(int startingValue){
         int total = startingValue;
         value = 0;
     }
 
+    //Post: value parameter is added to the total intstance variable
     public int add(int value){
         int total = total + value;
         return total;
     }
 
-    /**
-    * Adds the instance variable value to the total
-    */
+    //Post: value is added to the total instance variable
     public int add(){
         int total += value;
         return total;
     }
 
+    //Post: total instance variable is multiplied by value
     public int multiple(int value){
         int total *= value;
         return total;
     }
 
+    //Post: Sets the value instance variable
     public void setValue(int value){
         value = value;
     }
 
+    //Post: Gets the value instance variable
     public int getValue(){
         return value;
     }
